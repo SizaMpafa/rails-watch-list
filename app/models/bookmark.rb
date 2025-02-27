8bfe618d26990ebbc9b20425784050ce2165987c
SizaMpafa <sizampafa972@gmail.com>
2025-02-27 15:21:39 +0200

    moving to RCV part of MR CV

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,2 +1,7 @@
 class Bookmark < ApplicationRecord
+  belongs_to :movie
+  belongs_to :list
+  validates :comment, length: { is: 6 }
+  validates :movie_id, uniqueness: true
+  validates :list_id, uniqueness: true
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,4 @@
 class Movie < ApplicationRecord
+  has_many :bookmarks, dependent: :destroy
+  validates :title, :overview, uniqueness: true
 end
